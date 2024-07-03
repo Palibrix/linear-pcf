aba39e5c4e86d4a31bf103daec967ad577c016b8
palibrix <palibrix@gmail.com>
2024-07-03 17:12:52 +0300

    Add APScheduler for notifications to inactive users

--- flows/pizza/pizza.py
+++ flows/pizza/pizza.py
@@ -3,13 +3,12 @@ import re
 
 from dotenv import load_dotenv
 
-from pysyun.conversation.flow.console_bot import ConsoleBot
-from pysyun.conversation.flow.telegram_bot import TelegramBot
+from pysyun.conversation.flow.persistent_telegram_bot import PersistentTelegramBot
 
 load_dotenv()
 
 
-class PizzaBot(TelegramBot):
+class PizzaBot(PersistentTelegramBot):
 
     def build_state_machine(self, builder):
         main_menu_transition = self.build_menu_response_transition(
@@ -45,26 +44,27 @@ class PizzaBot(TelegramBot):
             .edge("/start", "/order", "Order Pizza", on_transition=order_pizza_transition) \
             .edge("/start", "/view_cart", "View Cart", on_transition=view_cart_transition) \
             .edge("/start", "/cancel_order", "Cancel Order", on_transition=cancel_order_transition) \
-        \
+ \
             .edge("/order", "/custom_pizza", "Custom Pizza", on_transition=custom_pizza_transition) \
             .edge("/order", "/start", "Back", on_transition=main_menu_transition) \
             .edge("/order", "/add_to_cart", None, matcher=re.compile(".*"), on_transition=add_to_cart_transition) \
-        \
+ \
             .edge("/custom_pizza", "/add_to_cart", None, matcher=re.compile(".*"),
                   on_transition=add_to_cart_transition) \
             .edge("/custom_pizza", "/order", "Back", on_transition=order_pizza_transition) \
-        \
+ \
             .edge("/add_to_cart", "/order", "Back", on_transition=order_pizza_transition) \
-        \
+ \
             .edge("/view_cart", "/start", "Back", on_transition=main_menu_transition) \
-        \
+ \
             .edge("/cancel_order", "/start", "Back", on_transition=main_menu_transition)
 
     def build_menu_response_transition(self, title, menu_items):
         menu = self.build_menu(menu_items)
 
         async def transition(action):
-            await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"],
+            chat_id = action["update"]["effective_chat"]["id"]
+            await action["context"].bot.send_message(chat_id=chat_id,
                                                      text=title,
                                                      reply_markup=menu)
 
@@ -73,7 +73,8 @@ class PizzaBot(TelegramBot):
     @staticmethod
     def build_message_response_transition(message):
         async def transition(action):
-            await action["context"].bot.send_message(chat_id=action["update"]["effective_chat"]["id"],
+            chat_id = action["update"]["effective_chat"]["id"]
+            await action["context"].bot.send_message(chat_id=chat_id,
                                                      text=message)
 
         return transition
